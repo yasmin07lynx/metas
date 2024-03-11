987ed3087c7c9fb31d3b34847c9315e65f973439
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-03-11 10:05:04 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const botoes = document.querySelectorAll ("botao");
-console.log (botoes)
 
-for (let i = 0; i < botoes.length; i++) {
-botoes [i]. onclick = function () {
-    for (let j = 0; j <botoes.length;j++) {
+for (let i=0; i <botoes.length; i++){
+botoes [i].onclick = function(){
+    for(let j = 0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo");
