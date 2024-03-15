9a01a8618939e60171fdd4aa78d5069a9ff28e50
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-03-15 11:11:29 -0300

    ...

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
-
+const texto = document.querySelectorAll(".aba-conteudo");
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            texto[j].classList.remove("ativo"); 
         }
+        
         botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
     }
 }
