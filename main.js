b02560fb0ca02f4227787b0e57b3751cc2f164d4
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-04-01 10:36:09 -0300

    sdfghjk,

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor (tempoFinal/1000);
-let minutos = Math.floor (segundos/60);
-let horas = Math.floor (minutos/60);
-let dias = Math.floor (horas/24);
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
 segundos %= 60;
 minutos %= 60;
