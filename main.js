e86640e1c3def0965c8b44ac650ef1d0f2df397c
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-04-01 10:28:36 -0300

    .

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ for(let i=0;i <botoes.length;i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
 let tempoAtual = new Date();
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+//contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
-function calculaTempo(tempoObjetivo1){
+//function calculaTempo(tempoObjetivo1){
 
-}
+//}
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor (tempoFinal/1000);
 let minutos = Math.floor (segundos/60);
