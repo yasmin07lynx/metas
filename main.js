551e51dc57e4b2562bf0745cec36107d4284bccd
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-04-01 10:30:59 -0300

    .

--- main.js
+++ main.js
@@ -17,9 +17,7 @@ let tempoAtual = new Date();
 //contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 //function calculaTempo(tempoObjetivo1){
-
-//}
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor (tempoFinal/1000);
 let minutos = Math.floor (segundos/60);
 let horas = Math.floor (minutos/60);
@@ -30,3 +28,5 @@ minutos %= 60;
 horas %= 24;
 
 contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+
+//}
