1b31d06f9b4790e11ebe1f20b40025c23e2b79d2
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-04-19 11:17:02 -0300

    .

--- main.js
+++ main.js
@@ -1,37 +1,54 @@
-const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo");
-for(let i=0;i <botoes.length;i++){
+const botoes = document.querySelectorAll (".botao");
+const texto = document. querySelectorAll (".aba-conteudo");
+for (let i=0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for(let j=0;j<botoes.length;j++){
+        for(let j = 0; j <botoes.length; j++){
             botoes[j].classList.remove("ativo");
-            texto[j].classList.remove("ativo"); 
+            texto[j].classList.remove("ativo");
         }
-        
         botoes[i].classList.add("ativo");
         texto[i].classList.add("ativo");
     }
+
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-21T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-21T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3];
 
 
-for (let i = 0; i < contadores.length; i++){
-contadores[i].textContent = calculaTempo(tempos[0]);
-}
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor(minutos/60);
-let dias = Math.floor(horas/24);
-
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-
-return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    
+    segundos %= 60;
+    minutos %=60;
+    horas %=24;
+     
+    if (tempoFinal > 0){
+        return dias + " dias " +horas+ " horas " +minutos+ " minutos " +segundos+ " segundos ";
+    } else{
+        return "Prazo Finalizado";
+    }
 }
-//comecaCronometro();
+
+function atualizaCronometro(){
+    for (let i=0;i < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+        }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
